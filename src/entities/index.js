de81d83a7a11ede4e10f79038689c8c5177454fd
JoaoPedroLanca <joaolanca53@gmail.com>
2025-12-01 17:52:55 -0300

    feat: Corrigi bug de import do sequelize

--- src/entities/index.js
+++ src/entities/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = require("../config/database");
+const { sequelize } = require("../config/database");
 
 const OrderEntity = require("./Order");
 const ItemEntity = require("./Item");
