b5b00f740b7896a66991e8c2d01691b70958abb1
JoaoPedroLanca <joaolanca53@gmail.com>
2025-12-01 17:31:04 -0300

    refactor: Refatora App.js incluindo o swagger e as rotas auth]

--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 require("dotenv").config();
 const express = require("express");
 const orderRoutes = require("./routes/orderRoutes");
+const authRoutes = require("./routes/authRoutes")
 const errorHandler = require("./middlewares/errorHandler");
+const { swaggerSpec } = require("./config/swagger");
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 app.get("/helth", (req, res) => {
     res.status(200).json({ status: 'ok' });
 });
+
+app.use("/auth", authRoutes);
 
 app.use("/order", orderRoutes);
 
