504640e32bb9301038652c8f8175e1d1b7805b71
JoaoPedroLanca <joaolanca53@gmail.com>
2025-12-01 17:45:52 -0300

    feat: Adiciona Schemas ao swagger.js para melhor documentação

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -27,6 +27,57 @@ const options = {
                     bearerFormat: 'JWT',
                     description: 'Insira o token JWT no formato: Bearer {token}'
                 }
+            },
+            schemas: {
+                Order: {
+                    type: 'object',
+                    properties: {
+                        orderId: {
+                            type: 'string',
+                            example: 'v10089016vdb'
+                        },
+                        value: {
+                            type: 'number',
+                            example: 10000
+                        },
+                        creationDate: {
+                            type: 'string',
+                            format: 'date-time',
+                            example: '2023-07-19T12:24:11.529Z'
+                        },
+                        items: {
+                            type: 'array',
+                            items: {
+                                $ref: '#/components/schemas/Item'
+                            }
+                        }
+                    }
+                },
+                Item: {
+                    type: 'object',
+                    properties: {
+                        itemId: {
+                            type: 'integer',
+                            example: 1
+                        },
+                        orderId: {
+                            type: 'string',
+                            example: 'v10089016vdb'
+                        },
+                        productId: {
+                            type: 'string',
+                            example: '2434'
+                        },
+                        quantity: {
+                            type: 'integer',
+                            example: 1
+                        },
+                        price: {
+                            type: 'number',
+                            example: 1000
+                        }
+                    }
+                }
             }
         },
         security: [
